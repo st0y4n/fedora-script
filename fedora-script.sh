fe55e184a087e39f25a53cedbe75311caef49bf2
Stoyan <stoyang2005@gmail.com>
2025-12-31 14:11:49 +0200

    .

--- fedora-script.sh
+++ fedora-script.sh
@@ -128,5 +128,24 @@ sudo dnf upgrade -y
 
 echo "If you've installed NVIDIA drivers open grub config "/etc/default/grub" and add "nvidia-drm.modeset=1" to "GRUB_CMDLINE_LINUX" line and then do "sudo grub2-mkconfig -o /boot/grub2/grub.cfg""
 
+echo "Setting up timeshift..."
+sudo sed -i -E '/\sbtrfs\s/ s/(\S+\s+\S+\s+btrfs\s+)(\S+)/\1\2,defaults,noatime,discard=async/' /etc/fstab
+sudo dnf install inotify-tools timeshift -y
+cd ~
+git clone https://github.com/Antynea/grub-btrfs
+cd grub-btrfs
+sed -i 's/^#GRUB_BTRFS_SUBMENUNAME=.*/GRUB_BTRFS_SUBMENUNAME="Fedora Linux snapshots"/' ./config
+sed -i 's/^#GRUB_BTRFS_SNAPSHOT_KERNEL_PARAMETERS=.*/GRUB_BTRFS_SNAPSHOT_KERNEL_PARAMETERS="rd.live.overlay.overlayfs=1"/' ./config
+sed -i 's/^#GRUB_BTRFS_GRUB_DIRNAME=.*/GRUB_BTRFS_GRUB_DIRNAME="\/boot\/grub2"/' ./config
+sed -i 's/^#GRUB_BTRFS_BOOT_DIRNAME=.*/GRUB_BTRFS_BOOT_DIRNAME="\/boot"/' ./config
+sed -i 's/^#GRUB_BTRFS_MKCONFIG=.*/GRUB_BTRFS_MKCONFIG=\/usr\/sbin\/grub2-mkconfig/' ./config
+sed -i 's/^#GRUB_BTRFS_SCRIPT_CHECK=.*/GRUB_BTRFS_SCRIPT_CHECK=grub2-script-check/' ./config
+sudo make install
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo cp /usr/lib/systemd/system/grub-btrfsd.service /etc/systemd/system/grub-btrfsd.service
+sudo sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/grub-btrfsd --syslog --timeshift-auto|' /etc/systemd/system/grub-btrfsd.service
+sudo systemctl enable --now grub-btrfsd
+
 echo "Reloading Zsh configuration..."
 exec zsh
+
